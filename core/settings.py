fca2e4edfb297b6633eb8cdf058962c98c0106fe
RABS <rizkyabdulbakkahshiddiq@gmail.com>
2026-09-22 06:14:52 +0700

    feat: update security settings in Django configuration

--- core/settings.py
+++ core/settings.py
@@ -16,8 +16,13 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["spondylitic-selenographically-seth.ngrok-free.dev", "localhost", "*"]
 
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 
 # Application definition
 
