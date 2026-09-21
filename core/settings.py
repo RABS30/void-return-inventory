507b5e36ef1c8a698f6b3e1dac8551fedfd33583
RABS <rizkyabdulbakkahshiddiq@gmail.com>
2026-09-22 06:45:18 +0700

    update security

--- core/settings.py
+++ core/settings.py
@@ -14,15 +14,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["spondylitic-selenographically-seth.ngrok-free.dev", "localhost", "*"]
+ALLOWED_HOSTS = ["void-return-inventory.rabs.cloud", "34.68.4.69"]
 
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Application definition
 
